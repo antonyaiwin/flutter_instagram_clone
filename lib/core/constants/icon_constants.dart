89463ae207e7d7d9c4cfd1a5c0ddde333dc2fcf9
Antony Aiwin K X <antonyaiwin007@gmail.com>
2024-02-29 02:40:59 +0530

    added icons and images

---
+++ lib/core/constants/icon_constants.dart
@@ -0,0 +1,43 @@
+class MyIcons {
+  static const String _iconsLightDir = 'assets/icons_light';
+  static const String _iconsDarkDir = 'assets/icons_dark';
+// icons constants
+// Home
+  static const String homeIconFilledPng = '$_iconsLightDir/home_filled.png';
+  static const String homeIconOutlinedPng = '$_iconsLightDir/home_outlined.png';
+
+// Search
+  static const String searchIconFilledPng = '$_iconsLightDir/search_filled.png';
+  static const String searchIconOutlinedPng =
+      '$_iconsLightDir/search_outlined.png';
+
+// Like
+  static const String likeIconFilledPng = '$_iconsLightDir/like_filled.png';
+  static const String likeIconOutlinedPng = '$_iconsLightDir/like_outlined.png';
+
+// New post
+  static const String newPostIconFilledPng =
+      '$_iconsLightDir/new_post_outlined.png';
+
+  // Camera
+  static const String cameraIconFilledPng = '$_iconsLightDir/camera_filled.png';
+  static const String cameraIconOutlinedPng =
+      '$_iconsLightDir/camera_outlined.png';
+
+  // Camera
+  // static const String commentIconFilledPng = '$_iconsLightDir/comment_filled.png';
+  static const String commentIconOutlinedPng =
+      '$_iconsLightDir/comment_outlined.png';
+
+  // Camera
+  // static const String saveIconFilledPng = '$_iconsLightDir/save_filled.png';
+  static const String saveIconOutlinedPng = '$_iconsLightDir/save_outlined.png';
+
+  // IGNY
+  static const String igtvIconFilledPng = '$_iconsLightDir/igtv_filled.png';
+  static const String igtvIconOutlinedPng =
+      '$_iconsDarkDir/igtv_outlined_dark.png';
+
+  static const String messengerOutlinedPng =
+      '$_iconsLightDir/messenger_outlined.png';
+}
