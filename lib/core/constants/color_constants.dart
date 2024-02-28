221ea859e03e46378d570fd66563abdff05a25b9
Antony Aiwin K X <antonyaiwin007@gmail.com>
2024-02-29 02:41:45 +0530

    added color in color_constants

--- lib/core/constants/color_constants.dart
+++ lib/core/constants/color_constants.dart
@@ -4,6 +4,8 @@ class ColorConstants {
   static const Color primaryBlue = Color(0xff3797EF);
   static const Color primaryWhite = Color(0xffffffff);
   static const Color primaryBlack = Color(0xff000000);
-  static const Color acountTextColor = Color(0xff262626);
-  static const Color black40 = Color(0xff262626);
+  static const Color black26 = Color(0xff262626);
+
+  // textField colors
+  static const Color textFieldFill = Color(0xffFAFAFA);
 }
