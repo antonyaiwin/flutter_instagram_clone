6259882c37202d96688aa3a071cacd0ae7dbf883
Antony Aiwin K X <antonyaiwin007@gmail.com>
2024-02-29 21:30:40 +0530

    added package flutter_web_frame for defining max width in web devices and enabled drag scrolling in web devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/view/splash_screen/splash_screen.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +15,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SplashScreen(),
+    return FlutterWebFrame(
+      builder: (context) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          scrollBehavior: MyCustomScrollBehavior(),
+          title: 'Instagram',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const SplashScreen(),
+        );
+      },
+      maximumSize: const Size(400.0, 812.0), // Maximum size
+      enabled: kIsWeb, // default is enable, when disable content is full size
+      backgroundColor: Colors.grey,
     );
   }
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
 }
