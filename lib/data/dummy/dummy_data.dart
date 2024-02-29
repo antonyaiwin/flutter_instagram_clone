f3d1efe182044644b13aba2562afae870b04f43f
Antony Aiwin K X <antonyaiwin007@gmail.com>
2024-02-29 20:46:36 +0530

    added some dummy data

---
+++ lib/data/dummy/dummy_data.dart
@@ -0,0 +1,149 @@
+import 'dart:math';
+
+class DummyData {
+  static List<String> firstNames = [
+    'Alice',
+    'Bob',
+    'Charlie',
+    'David',
+    'Emma',
+    'Frank',
+    'Grace',
+    'Henry',
+    'Ivy',
+    'Jack'
+  ];
+  static List<String> lastNames = [
+    'Smith',
+    'Johnson',
+    'Williams',
+    'Jones',
+    'Brown',
+    'Davis',
+    'Miller',
+    'Wilson',
+    'Moore',
+    'Taylor'
+  ];
+
+  static List<String> separators = ['.', '_'];
+
+  static String generateRandomUsername() {
+    String firstName = firstNames[Random().nextInt(firstNames.length)];
+    String lastName = lastNames[Random().nextInt(lastNames.length)];
+    String separator = separators[Random().nextInt(separators.length)];
+    String username = '$firstName$separator$lastName${Random().nextInt(100)}';
+
+    return username;
+  }
+
+  static String? generateRandomPlace() {
+    int index = Random().nextInt(placesAndCountries.length);
+    if (index.isEven) {
+      return null;
+    }
+    return placesAndCountries[index];
+  }
+
+  static List<String> realtimeComments = [
+    "Great content! 🚀",
+    "I learned a lot from this!",
+    "Can you explain the third point in more detail?",
+    "Awesome tutorial!",
+    "This is so helpful, thanks!",
+    "I have a question about the second paragraph.",
+    "Looking forward to more updates!",
+    "You make it so easy to understand.",
+    "Not clear on the example, can you provide another one?",
+    "I appreciate your effort in creating this.",
+    "Helpful information, thanks for sharing!",
+    "Excellent explanation!",
+    "I wish I found this sooner.",
+    "Any suggestions for further reading?",
+    "Very well written!",
+    "Clear and concise. Thanks!",
+    "Thanks for the valuable insights!",
+    "This is exactly what I was looking for.",
+    "Kudos to the author!",
+    "I love your writing style.",
+    "Could you cover topic X in your next post?",
+    "You've made a complex topic easy to grasp.",
+    "Thanks for the step-by-step guide!",
+    "I'm sharing this with my colleagues.",
+    "Can you do a tutorial on topic Y?",
+    "You've saved me a lot of time. Appreciate it!",
+    "I have a question about the conclusion.",
+    "Great examples provided!",
+    "Your expertise shines through. Well done!",
+    "I've bookmarked this page for future reference.",
+    "You've answered all my questions.",
+    "Do you have a newsletter I can subscribe to?",
+    "Mind-blowing insights!",
+    "I'm implementing this right away.",
+    "Thanks for the code snippets!",
+    "Could you elaborate on point number four?",
+    "You've simplified a complex subject. Bravo!",
+    "Can I contact you for further clarification?",
+    "I've never understood this concept until now.",
+    "You're a fantastic teacher!",
+    "I appreciate the real-world examples.",
+    "I'm recommending this to my peers.",
+    "Can you do a deep dive into topic Z?",
+    "Your writing is engaging and informative.",
+    "Thanks for sharing your knowledge!",
+    "I look forward to your future posts.",
+    "Can you provide more details on the third technique?",
+    "You've made a difference in my understanding.",
+  ];
+
+  static List<String> placesAndCountries = [
+    'Tokyo, Japan',
+    'Mumbai, India',
+    'New York, USA',
+    'London, UK',
+    'Paris, France',
+    'Beijing, China',
+    'Sydney, Australia',
+    'Cairo, Egypt',
+    'Rio de Janeiro, Brazil',
+    'Moscow, Russia',
+    'Berlin, Germany',
+    'Dubai, UAE',
+    'Seoul, South Korea',
+    'Toronto, Canada',
+    'Rome, Italy',
+    'Cape Town, South Africa',
+    'Bangkok, Thailand',
+    'Mexico City, Mexico',
+    'Hanoi, Vietnam',
+    'Lisbon, Portugal',
+    'Stockholm, Sweden',
+    'Buenos Aires, Argentina',
+    'Amsterdam, Netherlands',
+    'Singapore, Singapore',
+    'Istanbul, Turkey',
+    'Sydney, Australia',
+    'Nairobi, Kenya',
+    'Athens, Greece',
+    'Auckland, New Zealand',
+    'Copenhagen, Denmark',
+    'Helsinki, Finland',
+    'Oslo, Norway',
+    'Prague, Czech Republic',
+    'Vienna, Austria',
+    'Brussels, Belgium',
+    'Budapest, Hungary',
+    'Warsaw, Poland',
+    'Zurich, Switzerland',
+    'Madrid, Spain',
+    'Lima, Peru',
+    'Santiago, Chile',
+    'Manila, Philippines',
+    'Jakarta, Indonesia',
+    'Havana, Cuba',
+    'Panama City, Panama',
+    'Dakar, Senegal',
+    'Accra, Ghana',
+    'Kathmandu, Nepal',
+  ];
+}
--- lib/dummy_db.dart
+++ lib/dummy_db.dart
@@ -1,10 +1,14 @@
+import 'dart:math';
+
+import 'data/dummy/dummy_data.dart';
+
 class DummyDb {
   // dummy profile Picture URL
   static const String currentUserProfilePic =
       'https://images.pexels.com/photos/1812634/pexels-photo-1812634.jpeg?auto=compress&cs=tinysrgb&w=600';
 
 // Dummy avatar image URL List
-  static const List images = [
+  static const List personImages = [
     'https://images.pexels.com/photos/1520760/pexels-photo-1520760.jpeg?auto=compress&cs=tinysrgb&w=600',
     'https://images.pexels.com/photos/698532/pexels-photo-698532.jpeg?auto=compress&cs=tinysrgb&w=600',
     'https://images.pexels.com/photos/15019490/pexels-photo-15019490/free-photo-of-portrait-of-a-smiling-man.jpeg?auto=compress&cs=tinysrgb&w=600',
@@ -22,6 +26,24 @@ class DummyDb {
     'https://images.pexels.com/photos/3328058/pexels-photo-3328058.jpeg?auto=compress&cs=tinysrgb&w=600',
   ];
 
+  static const List postImages = [
+    'https://images.pexels.com/photos/20147180/pexels-photo-20147180/free-photo-of-a-close-up-of-a-blue-glacier.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/19172555/pexels-photo-19172555/free-photo-of-woman-with-black-hair-sitting-in-city.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/20116345/pexels-photo-20116345/free-photo-of-postal-altiplano-chileno.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/12428121/pexels-photo-12428121.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/4005498/pexels-photo-4005498.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/20420447/pexels-photo-20420447/free-photo-of-a-person-wearing-a-blue-hoodie-with-a-flower-in-their-pocket.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/11958479/pexels-photo-11958479.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/18509324/pexels-photo-18509324/free-photo-of-blooming-white-buttecup-flower.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/18119535/pexels-photo-18119535/free-photo-of-aerial-view-of-a-winding-road-in-the-forest.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/19859148/pexels-photo-19859148/free-photo-of-wild-roes-in-forest.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/8630759/pexels-photo-8630759.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/20113974/pexels-photo-20113974/free-photo-of-a-person-holding-a-cup-of-coffee-on-a-white-blanket.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/20330740/pexels-photo-20330740/free-photo-of-a-dog-with-light-brown-fur-sitting-on-a-meadow.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/19399844/pexels-photo-19399844/free-photo-of-young-woman-in-white-blouse-and-black-waistcoat.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+    'https://images.pexels.com/photos/12672768/pexels-photo-12672768.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+  ];
+
   // Dummy Story List
   static List<Map<String, dynamic>> storyList = [
     {
@@ -30,54 +52,77 @@ class DummyDb {
       'is_live': false,
     },
     {
-      'profile_pic': images[0],
+      'profile_pic': personImages[0],
       'user_name': 'John Doe',
       'is_live': true,
     },
     {
-      'profile_pic': images[1],
+      'profile_pic': personImages[1],
       'user_name': 'Alice Smith',
       'is_live': true,
     },
     {
-      'profile_pic': images[2],
+      'profile_pic': personImages[2],
       'user_name': 'Bob Johnson',
       'is_live': true,
     },
     {
-      'profile_pic': images[3],
+      'profile_pic': personImages[3],
       'user_name': 'Emily Davis',
       'is_live': false,
     },
     {
-      'profile_pic': images[4],
+      'profile_pic': personImages[4],
       'user_name': 'Daniel Wilson',
       'is_live': false,
     },
     {
-      'profile_pic': images[5],
+      'profile_pic': personImages[5],
       'user_name': 'Sophia Brown',
       'is_live': false,
     },
     {
-      'profile_pic': images[6],
+      'profile_pic': personImages[6],
       'user_name': 'Matthew Miller',
       'is_live': false,
     },
     {
-      'profile_pic': images[7],
+      'profile_pic': personImages[7],
       'user_name': 'Olivia Jones',
       'is_live': false,
     },
     {
-      'profile_pic': images[8],
+      'profile_pic': personImages[8],
       'user_name': 'William Garcia',
       'is_live': false,
     },
     {
-      'profile_pic': images[9],
+      'profile_pic': personImages[9],
       'user_name': 'Emma Martinez',
       'is_live': false,
     },
   ];
+
+  static List<Map<String, dynamic>> get postsList {
+    List users = [];
+    users.addAll(storyList);
+    users.removeAt(0);
+    users.shuffle();
+    return List.generate(50, (index) {
+      Map user = storyList[Random().nextInt(storyList.length)];
+      return {
+        'user_name': user['user_name'],
+        'place': DummyData.generateRandomPlace(),
+        'profile_pic': user['profile_pic'],
+        'post_image': postImages[Random().nextInt(postImages.length)],
+        'liked_user': DummyData.generateRandomUsername(),
+        'liked_user_pic': personImages[Random().nextInt(personImages.length)],
+        'commented_user': DummyData.generateRandomUsername(),
+        'hilight_comment': DummyData.realtimeComments[
+            Random().nextInt(DummyData.realtimeComments.length)],
+        'like_count': Random().nextInt(9999999) + 5,
+        'comment_count': Random().nextInt(9999999) + 5,
+      };
+    });
+  }
 }
